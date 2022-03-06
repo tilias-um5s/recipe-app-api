a788f61b80ebaa6bb8f5798e6e4567013080f09a
tilias-um5s <ilias.tougui@um5s.net.ma>
2022-03-06 16:21:49 +0100

    fixed linting errors of flake8

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -85,7 +85,7 @@ class PrivateTagsApiTests(TestCase):
         ).exists()
 
         self.assertTrue(exists)
-    
+
     def test_retrieve_tags_assigned_to_recipes(self):
         """Test filtering tags by those assigned to recipes"""
         tag1 = Tag.objects.create(user=self.user, name='Breakfast')
